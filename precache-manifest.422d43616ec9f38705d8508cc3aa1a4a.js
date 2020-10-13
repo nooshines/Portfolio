8f4980ea6e221d12fbf10ef949898f761cb96611
nooshines <nooshin_es26413@yahoo.com>
2020-10-13 13:27:56 +1100

    Updates

---
+++ precache-manifest.422d43616ec9f38705d8508cc3aa1a4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "689a64581c28dff1d745187012b3291b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e12428f890b3e458a8f7",
+    "url": "/portfolio/static/css/main.a6737c35.chunk.css"
+  },
+  {
+    "revision": "f54e85505b13e3e8cbba",
+    "url": "/portfolio/static/js/2.2208184f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.2208184f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e12428f890b3e458a8f7",
+    "url": "/portfolio/static/js/main.7241592d.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "32efcc4ccf2a51e9718df9339ae1799d",
+    "url": "/portfolio/static/media/icon2.32efcc4c.jpg"
+  }
+]);
